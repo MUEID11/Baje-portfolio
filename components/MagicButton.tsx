113d5c1261b57d3f84d836e5aa390b7031e9ced7
MUEID11 <mehedirangpur3@gmail.com>
2024-07-08 15:07:57 +0600

    fixed button

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { FaArrowDown } from "react-icons/fa";
-import { FaSortDown } from "react-icons/fa6";
+import { FaFileDownload } from "react-icons/fa";
 
 const MagicButton = () => {
   return (
-    <div className="flex flex-col sm:flex-row gap-4 mt-6">
-      <button className="relative inline-flex text-white items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md ">
-           <a 
-           href="https://drive.google.com/uc?export=download&id=1jgFsxPmH3gONvswq2SWJVZCJBI2mIION"
-           download="your-resume.pdf" className="inline-flex">Download Resume <FaSortDown className="ml-2" /></a>
-          </span>
-        </button>
+    <div className="flex flex-col sm:flex-row gap-4 mt-6 mr-4">
+      <a
+        href="https://drive.google.com/uc?export=download&id=1jgFsxPmH3gONvswq2SWJVZCJBI2mIION"
+        download="your-resume.pdf"
+        className="px-5 py-2.5 relative rounded group font-medium text-white inline-block text-sm"
+      >
+        <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+        <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+        <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+        <span className="relative flex justify-center items-center">
+          Download Resume <FaFileDownload className="ml-2" />
+        </span>
+      </a>
     </div>
   );
 };
