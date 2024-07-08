b9506ea252ca4730bf96621dfb0a7900cf57f67c
MUEID11 <mehedirangpur3@gmail.com>
2024-07-08 14:51:10 +0600

    button changed

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { FaArrowDown } from "react-icons/fa";
+import { FaSortDown } from "react-icons/fa6";
 
 const MagicButton = () => {
   return (
     <div className="flex flex-col sm:flex-row gap-4 mt-6">
-      <a
-        href="https://drive.google.com/uc?export=download&id=1jgFsxPmH3gONvswq2SWJVZCJBI2mIION"
-        download="your-resume.pdf"
-        className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-      >
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-          Download Resume <FaArrowDown className="text-lg ml-2" />
-        </span>
-      </a>
+      <button className="relative inline-flex text-white items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md ">
+           <a 
+           href="https://drive.google.com/uc?export=download&id=1jgFsxPmH3gONvswq2SWJVZCJBI2mIION"
+           download="your-resume.pdf" className="inline-flex">Download Resume <FaSortDown className="ml-2" /></a>
+          </span>
+        </button>
     </div>
   );
 };
